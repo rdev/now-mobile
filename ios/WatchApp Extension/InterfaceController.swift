6e41f305d0ba331e661fe48f7347cd30032c1265
Max <fivepointseven@icloud.com>
2018-07-06 01:42:34 +0800

    feat(watch): Basic set up for Watch app

---
+++ ios/WatchApp Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  WatchApp Extension
+//
+//  Created by Max Rovensky on 06/07/2018.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
